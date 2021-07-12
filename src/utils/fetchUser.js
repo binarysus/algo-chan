af97549b1283a7d04b6ae7993fd2535f33c265e3
Nicholas <me@cheeze2000.net>
2021-07-12 15:21:17 +0800

    refactor: move url to another file

--- src/utils/fetchUser.js
+++ src/utils/fetchUser.js
@@ -1,9 +1,8 @@
 const fetch = require("node-fetch");
-
-const BS_API_URL = "https://binarysearch.com/api/users/USERNAME/profile";
+const urls = require("../constants/urls.json");
 
 async function fetchUser(username) {
-  const url = BS_API_URL.replace("USERNAME", username);
+  const url = urls.BINARYSEARCH_API.replace("USERNAME", username);
   const data = await fetch(url).then(res => res.json());
 
   if ("user" in data) return data;
