c700597ef8072adc92aaf6a04cf4229b7c07349f
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2021-07-18 12:14:19 -0700

    refactor: returned data.user instead of user

--- src/util/fetchUser.ts
+++ src/util/fetchUser.ts
@@ -6,7 +6,7 @@ async function fetchUser(username: string): Promise<null | BSUser> {
   const url = urls.BINARYSEARCH_API.replace("USERNAME", username);
   const data = await fetch(url).then(res => res.json());
 
-  if ("user" in data) return data;
+  if ("user" in data) return data.user;
   return null;
 }
 
