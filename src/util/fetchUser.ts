cd3bd0299f541493fcdc6343a44d009f869e4054
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2021-07-20 10:12:31 -0700

    refactor: changed functione xport

--- src/util/fetchUser.ts
+++ src/util/fetchUser.ts
@@ -2,12 +2,10 @@ import fetch from "node-fetch";
 import BSUser from "../constants/bsUser";
 import urls from "../constants/urls";
 
-async function fetchUser(username: string): Promise<null | BSUser> {
+export default async function fetchUser(username: string): Promise<null | BSUser> {
   const url = urls.BINARYSEARCH_API.replace("USERNAME", username);
   const data = await fetch(url).then(res => res.json());
 
   if ("user" in data) return data.user;
   return null;
 }
-
-export default fetchUser;
