edf1aa1faa8e29d121f98e6190baddd6e6e881b8
itsUrcute <riniefrinoo@gmail.com>
2021-07-22 13:12:07 +0000

    perf: better performing startEventHandler function

--- src/internals/eventHandler.ts
+++ src/internals/eventHandler.ts
@@ -4,33 +4,53 @@ import { join } from "path";
 import type { Client, ClientEvents } from "discord.js";
 import type { DiscordEvent } from "../types/DiscordEvent";
 
-const events = new Collection<string, DiscordEvent>();
+async function startEventHandler(client: Client): Promise<Collection<string, DiscordEvent>> {
 
-async function startEventHandler(client: Client): Promise<void> {
+  const events = new Collection<string, DiscordEvent>();
   await loadFiles<DiscordEvent>(events, join(__dirname, "..", "events"));
 
-  const setEventsOn = new Set<keyof ClientEvents>(events.filter(x => !x.once).map(x => x.event));
-  const setEventsOnce = new Set<keyof ClientEvents>(events.filter(x => x.once).map(x => x.event));
+  const onEvents = new Collection<keyof ClientEvents, DiscordEvent[]>();
+  const onceEvents = new Collection<keyof ClientEvents, DiscordEvent[]>();
 
-  for (const e of setEventsOn) {
-    client.on<keyof ClientEvents>(e, (...p) => {
+  for (const [, eventFile] of events) {
+    if (eventFile.once) {
+      const vals = onceEvents.get(eventFile.event);
+      if (!vals) {
+        onceEvents.set(eventFile.event, [eventFile]);
+      }
+      else {
+        vals.push(eventFile);
+      }
+    }
+    else {
+      const vals = onEvents.get(eventFile.event);
+      if (!vals) {
+        onEvents.set(eventFile.event, [eventFile]);
+      }
+      else {
+        vals.push(eventFile);
+      }
+    }
+  }
 
-      events.filter(ev => ev.event === e).forEach(a => {
-        // @ts-expect-error types being weird
-        a.execute(...p);
-      });
+  for (const [event, val] of onEvents) {
+    client.on(event, (...params) => {
+      for (const dEvent of val) {
+        // @ts-expect-error Types being weird
+        dEvent.execute(...params);
+      }
     });
   }
 
-  for (const e of setEventsOnce) {
-    client.once<keyof ClientEvents>(e, (...p) => {
-
-      events.filter(ev => ev.event === e).forEach(a => {
-        // @ts-expect-error types being weird 2: electric boogaloo
-        a.execute(...p);
-      });
+  for (const [event, val] of onceEvents) {
+    client.once(event, (...params) => {
+      for (const dEvent of val) {
+        // @ts-expect-error Types being weird 2: Electric boogaloo
+        dEvent.execute(...params);
+      }
     });
   }
+  return events;
 }
 
 export {
