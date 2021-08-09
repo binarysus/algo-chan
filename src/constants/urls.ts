8dfb19487bf3bce069da27b28bbc81a6e4fe431c
Nicholas <me@cheeze2000.net>
2021-08-09 17:36:30 +0800

    feat: command to search cpp docs

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,3 +1,4 @@
 export default {
-  "BINARYSEARCH_API": "https://binarysearch.com/api/users/USERNAME/profile"
+  "BINARYSEARCH_API": "https://binarysearch.com/api/users/USERNAME/profile",
+  "CPPREFERENCE": "https://en.cppreference.com/w/cpp/"
 };
