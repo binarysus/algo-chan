311ecdac1c6661455d7e4db913698fa16af493d6
tOwOgarashi <togarashi@togarashi.owoowowowowowoowowowowo>
2021-08-11 11:06:29 -0700

    feat: add piston context menu handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { Client, Intents } from "discord.js";
+import { Client, Intents, Interaction, MessageEmbed, MessageAttachment } from "discord.js";
 import { startCommandHandler } from "#internals/commandHandler";
 import { startEventHandler } from "#internals/eventHandler";
+import { inspect } from "util";
 
 const client = new Client({
   intents: [
@@ -12,5 +13,61 @@ const client = new Client({
 
 startEventHandler(client);
 startCommandHandler(client);
+
+client.on("interaction", async(interaction: Interaction) => {
+  if(interaction.isContextMenu()) {
+    const api = "https://emkc.org/api/v1/piston/execute";
+    interface Fetched {
+     ran: boolean;
+     language: string;
+     version: string;
+     output: string;
+     stdout: string;
+     stderr: string;
+    }
+
+    const message = interaction.options.getMessage("message", true);
+
+    const code = message.content.match(/```[a-zA-Z+]*\n([\s\S]*?)```/);
+    const lang = message.content.replace(/\n/g, " ").split(/ +/).slice(3);
+
+    const body = {
+      "language": lang,
+      "source": code
+    };
+
+    const fetched: Fetched = await fetch(api, { method: "post", body:JSON.stringify(body), headers: { "Content-Type": "application/json" } }).then(res => res.json());
+    const resultEmbed = new MessageEmbed();
+
+    if(fetched.stderr) {
+      resultEmbed.setTitle("oops, there was an error")
+        .setDescription(`\`\`\`${fetched.stderr}\`\`\``);
+      if(fetched.stderr.length > 4000) {
+        const file = new MessageAttachment(Buffer.from(`${fetched.stderr}`), "piston.txt");
+        await interaction.reply({ content: "output was too long so it has been converted to a file", ephemeral: true });
+        await interaction.user.send({ files: [file] });
+      }
+      else{
+        await interaction.reply({ embeds: [resultEmbed], ephemeral: true });
+      }
+    }
+    else if(fetched.stdout) {
+      resultEmbed.setTitle(`successfully ran ${fetched.language}`)
+        .setDescription(`\`\`\`${lang}\n${fetched.stdout}\`\`\``);
+      if(fetched.stdout.length > 4000) {
+        const file = new MessageAttachment(Buffer.from(`${fetched.stdout}`), "piston.txt");
+        await interaction.reply({ content: "output was too long to fit into an embed to it has been converted to a file", ephemeral: true });
+        await interaction.user.send({ files: [file] });
+      }
+      else{
+        await interaction.reply({ embeds: [resultEmbed] });
+      }
+    }
+    else{
+      interaction.reply(`\`\`\`${inspect(fetched)}\`\`\``);
+    }
+
+  }
+});
 
 client.login(process.env.TOKEN);
