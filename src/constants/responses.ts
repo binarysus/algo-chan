ef910618046831e31e37ec016133b9cfca49b179
Urcute <75350256+itsUrcute@users.noreply.github.com>
2021-08-12 17:11:20 +0530

    feat: eslint revamp and prettier with eslint-config-marine  (#32)
    * feat: eslint revamp and prettier with eslint-config-marine
    
    * chore: formatting config files
    
    * chore: config files
    
    * chore: add npm scripts for formatting and checking formatting
    
    * style: format lint config files

--- src/constants/responses.ts
+++ src/constants/responses.ts
@@ -1,1 +1,2 @@
-export const PROFILE_NOT_FOUND = "Sorry, I could not find the user on binarysearch. Please note that usernames are case sensitive.";
+export const PROFILE_NOT_FOUND =
+	"Sorry, I could not find the user on binarysearch. Please note that usernames are case sensitive.";
--- src/types/Command.ts
+++ src/types/Command.ts
@@ -1,9 +1,9 @@
-import { ApplicationCommandData, ApplicationCommandPermissionData, CommandInteraction } from "discord.js";
+import type { ApplicationCommandData, ApplicationCommandPermissionData, CommandInteraction } from "discord.js";
 interface Command extends ApplicationCommandData {
-  nsfw?: boolean;
-  ownerOnly?: boolean;
-  permissions?: ApplicationCommandPermissionData[];
-  execute(interaction: CommandInteraction): Promise<void>;
+	nsfw?: boolean;
+	ownerOnly?: boolean;
+	permissions?: ApplicationCommandPermissionData[];
+	execute(interaction: CommandInteraction): Promise<void>;
 }
 
 export { Command };
