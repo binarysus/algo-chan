2f9925f7102c6704b7bd656d5f3a5fe7a98c81a5
Urcute <75350256+itsUrcute@users.noreply.github.com>
2021-08-23 07:59:22 +0530

    feat: logger (#39)
    * feat: button handler
    
    * feat: logger
    
    * chore: apply requested changes
    
    * style: fix indentation in tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { Client, Intents } from "discord.js";
+import { LogLevel, Logger } from "#classes/Logger";
 import { startButtonHandler } from "#internals/buttonHandler";
 import { startCommandHandler } from "#internals/commandHandler";
 import { startEventHandler } from "#internals/eventHandler";
 
+declare module "discord.js" {
+	interface Client {
+		logger: Logger;
+	}
+}
+
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]
 });
+
+client.logger = new Logger(LogLevel.ALL);
 
 startEventHandler(client);
 startCommandHandler(client);
